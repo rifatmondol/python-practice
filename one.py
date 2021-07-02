aeec70990e66a72b9e6a3ff59c0ca8a90074797f
rifat mondol <rifatmondol791@gmail.com>
2021-07-03 02:23:15 +0600

    first two and last two words

---
+++ one.py
@@ -0,0 +1,15 @@
+# 1. Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string.
+# If the string length is less than 2, return the empty string.
+string = input("Enter string:")
+count = 0
+
+if len(string) < 2:
+    print("Empty String")
+else:
+    for i in string:
+        print(i)            # Just for understanding loop
+        print("*" * 2)      # Just for understanding loop
+        count = count + 1
+
+    new = string[0:2] + string[count - 2:count]
+    print("New string is:", new)
